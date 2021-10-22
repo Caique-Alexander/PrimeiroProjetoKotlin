b3c88142db7e5e5559f101ffbacf2cb68d9be378
Caique Alexander de Carvalho <caiqacarvalho@gmail.com>
2021-10-22 12:11:15 -0300

    Criando Primeiro Projeto Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println("<???> Opa, eae")
+    println("De boas?")
+    println("Suaves")
+    println("<Rogério> Qual o seu nome?")
+    println("<???> Meu nome é Caique")
+    println("<Rogério> Opa, prazer em conhecê-lo Caique")
+    println("<Caique> O prazer é todo meu ${args.joinToString()}")
+}
